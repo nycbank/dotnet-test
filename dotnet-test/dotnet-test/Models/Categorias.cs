8f4a2b7ba98cb794138583dc1dbe1fedc70171f5
Marcos Smeets <48696729+MarcosSmeets@users.noreply.github.com>
2023-07-11 15:52:37 -0300

    Adicionando Models

---
+++ dotnet-test/dotnet-test/Models/Categorias.cs
@@ -0,0 +1,7 @@
+namespace dotnet_test.Models;
+
+public class Categorias
+{
+    public int id { get; set; }
+    public string nome { get; set; }
+}
---
+++ dotnet-test/dotnet-test/Models/Produtos.cs
@@ -0,0 +1,8 @@
+namespace dotnet_test.Models;
+
+public class Produtos
+{
+    public int id { get; set; }
+    public string nome { get; set; }
+    public decimal preco { get; set; }
+}
