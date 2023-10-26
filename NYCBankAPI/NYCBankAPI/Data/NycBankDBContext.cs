bbd113082ebba92f0115a85aa6b5f64b017d4e44
Gustavo Figueiroa <gustavofsoledade@hotmail.com>
2023-10-25 23:39:59 -0300

    feat: Creating database context class

---
+++ NYCBankAPI/NYCBankAPI/Data/NycBankDBContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NYCBankAPI.Models;
+
+namespace NYCBankAPI.Data;
+
+public class NycBankDBContext : DbContext
+{
+    public NycBankDBContext(DbContextOptions<NycBankDBContext> options) : base(options)
+    {
+    }
+
+    public DbSet<ProductModel> Products { get; set; }
+    public DbSet<CategoryModel> Categories { get; set; }
+
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+    }
+
+}
