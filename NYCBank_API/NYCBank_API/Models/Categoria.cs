f7baf8e3d24ed951ea0d9d95f565188c140ee0df
André Toti <detoti@gmail.com>
2023-10-26 18:09:02 -0300

    feat(Categoria): Categoria class model created.

---
+++ NYCBank_API/NYCBank_API/Models/Categoria.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+namespace NYCBank_API.Models;
+
+public class Categoria
+{
+	[Key]
+	[Required]
+	public int Id { get; set; }
+
+	[Required(ErrorMessage = "O nome da categoria é obrigatório.")]
+    [MaxLength(50, ErrorMessage = "O nome da categoria não pode exceder 50 caracteres")]
+    public string Nome { get; set; }
+
+}
+
