abd9f66d07d76f867d28979fce039a4bca802fc4
André Toti <detoti@gmail.com>
2023-10-26 11:14:53 -0300

    feat(HttpPost): HttpPost method working and initial "Produto" parameters set.

---
+++ NYCBank_API/NYCBank_API/Controllers/ProdutosController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+using NYCBank_API.Models;
+
+namespace NYCBank_API.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+
+public class ProdutosController : ControllerBase
+{
+	private static List<Produto> produtos = new List<Produto>();
+
+	[HttpPost]
+	public void AdicionaProduto([FromBody] Produto produto)
+	{
+            produtos.Add(produto);
+	}
+}
+
---
+++ NYCBank_API/NYCBank_API/Models/Produto.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+namespace NYCBank_API.Models;
+
+public class Produto
+{
+    [Required(ErrorMessage = "O nome do produto é obrigatório.")]
+    [MaxLength(50, ErrorMessage = "O nome do produto não pode exceder 50 caracteres")]
+    public string Nome { get; set; }
+
+    [Required(ErrorMessage = "O preço do produto é obrigatório.")]
+    [Range(0.01, 10000, ErrorMessage = "O preço deve ser maior que R$0.01 e menor que R$10.000")]
+    public decimal Preco { get; set; }
+
+    [Required(ErrorMessage = "A categoria do produto é obrigatório.")]
+    public string Categoria { get; set; }
+
+}
+
