dc5c4d827b279911b434d2f5685e47c84e2f9420
thais-ads <thaiss.spinoza@gmail.com>
2023-10-30 18:52:57 -0300

    feat: adaptaçoes cors

--- NycbankAPI/NycbankAPI/Program.cs
+++ NycbankAPI/NycbankAPI/Program.cs
@@ -1,7 +1,14 @@
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.EntityFrameworkCore;
+using NycBank.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+var stringConexao = "User Id=rm95546;Password=170599;Data Source=(DESCRIPTION=(ADDRESS_LIST=(ADDRESS=(PROTOCOL=TCP)(HOST=oracle.fiap.com.br)(PORT=1521)))(CONNECT_DATA=(SERVICE_NAME=ORCL)))";
 
+builder.Services.AddDbContext<BankContext>(options =>
+    options.UseOracle(stringConexao));
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
