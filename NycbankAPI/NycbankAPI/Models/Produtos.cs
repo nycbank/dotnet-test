698c21e4dd17b7a6207203eb106c5d1f4540ec70
thais-ads <thaiss.spinoza@gmail.com>
2023-10-30 19:06:33 -0300

    feat: create modal produtos

---
+++ NycbankAPI/NycbankAPI/Models/Produtos.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Xml.Linq;
+
+namespace NycbankAPI.Models
+{
+    [Table("Produtos")]
+    public class Produtos
+    {
+        [Key]
+        [Column]
+        [Display(Name = "Codigo")]
+        public int ID { get; set; }
+        [Column]
+        public string Nome { get; set; }
+        [Column]
+        public double Preco { get; set; }
+
+        public int CategoriaId { get; set; }
+
+
+        public Categoria Categoria { get; set; }
+    }
+}
