2682ecf1244cd3fde2a2a464e2e6c4195c08a82c
Lucas Leôncio <wey.lucas1@gmail.com>
2023-10-31 11:11:22 -0300

    Criação de Tasks do CRUD relacionadas a tabela Categorias

---
+++ dotnet-test/Repositorios/Interfaces/ICategoriaRepositorio.cs
@@ -0,0 +1,14 @@
+﻿using dotnet_test.Models;
+
+namespace dotnet_test.Repositorios.Interfaces
+{
+    public interface ICategoriaRepositorio
+    {        
+            Task<List<Categoria>> BuscarTodasCategorias();
+            Task<Categoria> BuscarPorId(int id);
+            Task<Categoria> Adicionar(Categoria categoria);
+            Task<Categoria> Atualizar(Categoria categoria, int id);
+            Task<bool> Apagar(int id);
+        
+    }
+}
