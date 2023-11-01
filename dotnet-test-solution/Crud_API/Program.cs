77edddc1378c7f569f84557b4dc55f40b113f468
Jonatas6199 <44883581+Jonatas6199@users.noreply.github.com>
2023-10-31 21:19:07 -0300

    initialize dependency injection into program.cs

--- dotnet-test-solution/Crud_API/Program.cs
+++ dotnet-test-solution/Crud_API/Program.cs
@@ -1,3 +1,9 @@
+using Repository.DatabaseContexts;
+using Repository.Interfaces;
+using Repository.Repositories;
+using Service.Interfaces;
+using Service.Services;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +12,16 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+//Add database context
+builder.Services.AddDbContext<RegisterContext>();
+
+//Add dependency injection
+builder.Services.AddScoped<IProductRepository, ProductRepository>();
+builder.Services.AddScoped<ICategoryRepository, CategoryRepository>();
+builder.Services.AddScoped<IProductCategoryRepository, ProductCategoryRepository>();
+builder.Services.AddScoped<ICategoryService, CategoryService>();
+builder.Services.AddScoped<IProductService, ProductService>();
 
 var app = builder.Build();
 
